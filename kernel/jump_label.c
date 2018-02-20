288f5dbaae790fdbda8d626b7b662cb97a68e9d8
Josh Poimboeuf <jpoimboe@redhat.com>
2018-02-20 11:37:53 -0600

    extable: Make init_kernel_text() global
    Convert init_kernel_text() to a global function and use it in a few
    places instead of manually comparing _sinittext and _einittext.
    
    Note that kallsyms.h has a very similar function called
    is_kernel_inittext(), but its end check is inclusive.  I'm not sure
    whether that's intentional behavior, so I didn't touch it.
    
    Suggested-by: Jason Baron <jbaron@akamai.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/4335d02be8d45ca7d265d2f174251d0b7ee6c5fd.1519051220.git.jpoimboe@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -429,8 +429,7 @@ void __init jump_label_invalidate_init(void)
 	struct jump_entry *iter;
 
 	for (iter = iter_start; iter < iter_stop; iter++) {
-		if (iter->code >= (unsigned long)_sinittext &&
-		    iter->code < (unsigned long)_einittext)
+		if (init_kernel_text(iter->code))
 			iter->code = 0;
 	}
 }
